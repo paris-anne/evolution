49fea4acb5e791da0ea00a92b1eb183a6f754b62
paris-anne <32743422+paris-anne@users.noreply.github.com>
2017-10-16 13:16:00 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import agent as ag
+import environment as env
+
+enviro = env.Environment(0.1)
+
+agent = Agent([0,0], 0.5, 1.0, enviro)
+enviro.add_agent(agent.pos())
+
+for i in range(100):
+	agent.living_cycle()
+
