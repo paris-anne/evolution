27c78e74d57fb255462c4c5477ddc7bd706abae8
hamed-97 <HK2715@ic.ac.uk>
2017-10-31 14:17:25 +0000

    funiform

--- main.py
+++ main.py
@@ -2,14 +2,31 @@ import agent as ag
 import numpy as np
 import environment as env
 
-enviro = env.Environment(100.0, 100.0)
-enviro.add_agents(100)
-enviro.addfood(25.0, 25.0, 5)
-enviro.addfood(25.0, 75.0, 5)
-enviro.addfood(75.0, 75.0, 5)
-enviro.addfood(75.0, 25.0, 5)
-enviro.display(1000)
+envirox = 1000.0
+enviroy = 1000.0
+foodnumber = 225
+enviro = env.Environment(envirox, enviroy)
+enviro.add_agents(49)
 
+# for i in np.arange(1,foodnumber,1):
+# 		enviro.addfood(envirox *i/foodnumber, enviroy*i/foodnumber, 5)
+
+for i in np.arange(0,envirox, envirox/np.sqrt(foodnumber)):
+	for j in np.arange(0, enviroy, enviroy/np.sqrt(foodnumber)):
+		print (i,j)
+		enviro.addfood(i,j, 5)
+
+
+
+# enviro.addfood(50.0, 50.0, 5)
+# enviro.addfood(200.0, 200.0, 5)
+# enviro.addfood(100.0, 50, 5)
+# enviro.addfood(50.0, 50.0, 5)
+# enviro.addfood(125.0, 125.0, 5)
+
+
+
+enviro.display(100000)
 enviro.plot()
 Data = []
 
